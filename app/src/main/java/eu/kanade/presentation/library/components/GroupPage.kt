252f0ec9e70d7eca6deb7ed184d6240aa8264405
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-06-02 21:31:30 +0500

    [library] GroupPage accepts globalGroupPreference and globalGroupByPreference

--- app/src/main/java/eu/kanade/presentation/library/components/GroupPage.kt
+++ app/src/main/java/eu/kanade/presentation/library/components/GroupPage.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.getValue
 import tachiyomi.core.common.preference.Preference
 import tachiyomi.domain.library.model.LibraryGroup
 import tachiyomi.i18n.MR
+import tachiyomi.presentation.core.components.CheckboxItem
 import tachiyomi.presentation.core.components.RadioItem
 import tachiyomi.presentation.core.i18n.stringResource
 import tachiyomi.presentation.core.util.collectAsState
@@ -13,32 +14,69 @@ import tachiyomi.presentation.core.util.collectAsState
 @Composable
 fun ColumnScope.GroupPage(
     groupPreference: Preference<Int>,
+    globalGroupPreference: Preference<Boolean>,
+    globalGroupByPreference: Preference<Int>,
 ) {
-    val groupType by groupPreference.collectAsState()
+    val isGlobal by globalGroupPreference.collectAsState()
 
-    RadioItem(
-        label = stringResource(MR.strings.label_default),
-        selected = groupType == LibraryGroup.BY_DEFAULT,
-        onClick = { groupPreference.set(LibraryGroup.BY_DEFAULT) },
+    CheckboxItem(
+        label = stringResource(MR.strings.pref_library_group_apply_to_all),
+        pref = globalGroupPreference,
     )
-    RadioItem(
-        label = stringResource(MR.strings.action_group_by_source),
-        selected = groupType == LibraryGroup.BY_SOURCE,
-        onClick = { groupPreference.set(LibraryGroup.BY_SOURCE) },
-    )
-    RadioItem(
-        label = stringResource(MR.strings.action_group_by_status),
-        selected = groupType == LibraryGroup.BY_STATUS,
-        onClick = { groupPreference.set(LibraryGroup.BY_STATUS) },
-    )
-    RadioItem(
-        label = stringResource(MR.strings.action_group_by_track_status),
-        selected = groupType == LibraryGroup.BY_TRACK_STATUS,
-        onClick = { groupPreference.set(LibraryGroup.BY_TRACK_STATUS) },
-    )
-    RadioItem(
-        label = stringResource(MR.strings.action_group_ungrouped),
-        selected = groupType == LibraryGroup.UNGROUPED,
-        onClick = { groupPreference.set(LibraryGroup.UNGROUPED) },
-    )
+
+    if (isGlobal) {
+        val globalGroupType by globalGroupByPreference.collectAsState()
+        RadioItem(
+            label = stringResource(MR.strings.label_default),
+            selected = globalGroupType == LibraryGroup.BY_DEFAULT,
+            onClick = { globalGroupByPreference.set(LibraryGroup.BY_DEFAULT) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_source),
+            selected = globalGroupType == LibraryGroup.BY_SOURCE,
+            onClick = { globalGroupByPreference.set(LibraryGroup.BY_SOURCE) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_status),
+            selected = globalGroupType == LibraryGroup.BY_STATUS,
+            onClick = { globalGroupByPreference.set(LibraryGroup.BY_STATUS) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_track_status),
+            selected = globalGroupType == LibraryGroup.BY_TRACK_STATUS,
+            onClick = { globalGroupByPreference.set(LibraryGroup.BY_TRACK_STATUS) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_ungrouped),
+            selected = globalGroupType == LibraryGroup.UNGROUPED,
+            onClick = { globalGroupByPreference.set(LibraryGroup.UNGROUPED) },
+        )
+    } else {
+        val groupType by groupPreference.collectAsState()
+        RadioItem(
+            label = stringResource(MR.strings.label_default),
+            selected = groupType == LibraryGroup.BY_DEFAULT,
+            onClick = { groupPreference.set(LibraryGroup.BY_DEFAULT) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_source),
+            selected = groupType == LibraryGroup.BY_SOURCE,
+            onClick = { groupPreference.set(LibraryGroup.BY_SOURCE) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_status),
+            selected = groupType == LibraryGroup.BY_STATUS,
+            onClick = { groupPreference.set(LibraryGroup.BY_STATUS) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_by_track_status),
+            selected = groupType == LibraryGroup.BY_TRACK_STATUS,
+            onClick = { groupPreference.set(LibraryGroup.BY_TRACK_STATUS) },
+        )
+        RadioItem(
+            label = stringResource(MR.strings.action_group_ungrouped),
+            selected = groupType == LibraryGroup.UNGROUPED,
+            onClick = { groupPreference.set(LibraryGroup.UNGROUPED) },
+        )
+    }
 }
