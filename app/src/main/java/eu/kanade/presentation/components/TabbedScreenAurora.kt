6c166f2efcc4363387ba6800ae0dc60dceee3b1c
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-06-21 16:01:51 +0500

    [ui] Remove Material ripple from AuroraTab clicks to prevent double-highlight on tap

--- app/src/main/java/eu/kanade/presentation/components/TabbedScreenAurora.kt
+++ app/src/main/java/eu/kanade/presentation/components/TabbedScreenAurora.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -866,7 +867,10 @@ internal fun AuroraTab(
     Box(
         modifier = modifier
             .clip(tabShape)
-            .clickable {
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+            ) {
                 appHaptics.tap()
                 onClick()
             }
